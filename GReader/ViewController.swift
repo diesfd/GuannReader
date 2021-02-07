92eb6bb4e89f1265eba800b29d963d36fdde9363
liyuetong <lyt1040755930@gmail.com>
2021-02-07 23:28:32 +0800

    Initial Commit

---
+++ GReader/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GReader
+//
+//  Created by Li Yuetong on 2/7/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
