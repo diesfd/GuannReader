9b3fe74427702051499ae9e371d92532c490bfe8
liyuetong <lyt1040755930@gmail.com>
2021-02-08 01:08:21 +0800

    Now the label can display the input.

--- GReader/ViewController.swift
+++ GReader/ViewController.swift
@@ -10,17 +10,19 @@ import Carbon.HIToolbox
 
 class ViewController: NSViewController {
     
+    @IBOutlet weak var inputTextLabel: NSTextField!
+    var inputBuffer = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         NSEvent.addLocalMonitorForEvents(matching: .keyDown) {
-                 if self.myKeyDown(with: $0) {
-                    return nil
-                 } else {
-                    return $0
-                 }
-              }
+            if self.myKeyDown(with: $0) {
+                return nil
+            } else {
+                return $0
+            }
+        }
     }
     
     override var representedObject: Any? {
@@ -33,17 +35,26 @@ class ViewController: NSViewController {
      * A glabal keyboard event listener.
      */
     func myKeyDown(with event: NSEvent) -> Bool {
-          // handle keyDown only if current window has focus, i.e. is keyWindow
-          guard let locWindow = self.view.window,
-             NSApplication.shared.keyWindow === locWindow else { return false }
-          switch Int( event.keyCode) {
-          case kVK_ANSI_J:
-            Swift.print("j")
-            return true
-          default:
-             return false
-          }
-       }
+        // handle keyDown only if current window has focus, i.e. is keyWindow
+        guard let locWindow = self.view.window,
+              NSApplication.shared.keyWindow === locWindow else { return false }
+        Swift.print(event.characters!)
+        if self.inputBuffer.count <= 20 {
+            self.inputBuffer.append(event.characters!)
+        } else {
+            self.inputBuffer = ""
+        }
+        self.inputTextLabel.stringValue = self.inputBuffer
+        
+        return true
+        //        switch Int( event.keyCode) {
+        //        case kVK_ANSI_J:
+        //            Swift.print("j")
+        //            return true
+        //        default:
+        //            return false
+        //        }
+    }
     
     
 }
